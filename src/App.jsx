644361d589bad34ad904e79be6007185cd2dab30
Muhammed demirci <82472683+Muhammeddemircii@users.noreply.github.com>
2025-04-21 16:05:19 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route,  Routes } from 'react-router-dom'
+import './App.css'
+import Dashboard from './pages/Dashboard'
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Dashboard" element={<Dashboard />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
